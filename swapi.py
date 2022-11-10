71ed94b767a926e41049fc9f9eca694f567a9597
alexis perez <alexisperez99@hotmail.fr>
2022-11-10 13:17:58 +0100

    test

--- swapi.py
+++ swapi.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import warnings
+import numpy as np
 warnings.filterwarnings('ignore')
 
 #Call the API
@@ -14,17 +15,18 @@ def page_number(number) :
     page=ceil(number/10)
     return page
 
+
 #get the number of entities
 def entity_number(data_dict) :
     a=data_dict['count']
     return a
 
 def bb(link,page):
-fd=pd.DataFrame()
-for x in range(1,page+1) :
-    request=requests.get(link+str(x))
-    data_dict = request.json()
-    species_df = data_dict['results']
-    species_df = pd.DataFrame(species_df)
-    fd=fd.append(species_df)
-return fd
+    fd=pd.DataFrame()
+    for x in range(1,page+1) :
+        request=requests.get(link+str(x))
+        data_dict = request.json()
+        species_df = data_dict['results']
+        species_df = pd.DataFrame(species_df)
+        fd=fd.append(species_df)
+    return fd
